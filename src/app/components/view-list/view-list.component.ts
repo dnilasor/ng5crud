19988f8c34d292e927ca37f019f798b423f4e9c2
Rosalind Benoit <dnilasor@localhost.localdomain>
2018-06-07 14:10:48 -0400

    there are errors but before I **** with the routing...

--- src/app/components/view-list/view-list.component.ts
+++ src/app/components/view-list/view-list.component.ts
@@ -1,20 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { TraineeService } from '../../trainee.service';
+import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule, NgControl } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import { Trainee } from './Trainee';
+import { Session } from './Session';
+import { SessionService } from '../../session.service';
 
 @Component({
   selector: 'app-view-list',
   templateUrl: './view-list.component.html',
   styleUrls: ['./view-list.component.css']
 })
-export class ViewListComponent implements OnInit {
-  onSelectChange() {
-    // do something
-  }
-  constructor() { 
-    this.selectedTrainess = this.ed
 
+export class ViewListComponent implements OnInit {
+
+  trainees: any;
+  sessions: any;
+  selected: any;
+  selectedTrainees: any;
+
+  constructor(private http: HttpClient, private service: TraineeService, private sessionservice: SessionService) {
+    this.selectedTrainees = this.trainees;
   }
 
   ngOnInit() {
+    this.getTrainees();
+    this.getSessions();
+  }
+  getTrainees() {
+    this.service.getTrainees().subscribe(res => {
+      this.trainees = res;
+    });
+  }
+  getSessions() {
+    this.sessionservice.getSessions().subscribe(res => {
+      this.sessions = res;
+    });
   }
 
+  onSelect(date) {
+    console.log(date);
+    this.selectedTrainees = this.trainees.filter(trainee => trainee.dateTime == date);
+  }
 }
